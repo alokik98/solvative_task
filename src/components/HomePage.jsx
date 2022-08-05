debc9c58f5871d0e7fa53c37246c9df182210e96
alokik98 <alorshsaxena12@gmail.com>
2022-08-05 14:31:19 +0530

    Created HomePage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./HomePage.css"
+
+const HomePage = () => {
+  return (
+    <>
+        <div className='search-box'>
+            <input type='text' placeholder='Search places ...' />
+        </div>
+        
+    </>
+  )
+}
+
+export default HomePage
