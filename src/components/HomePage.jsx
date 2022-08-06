ba56de730c630f780eb2973e286349b15f3ad7f6
alokik98 <alorshsaxena12@gmail.com>
2022-08-06 11:57:49 +0530

    Added limit feature

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import "./HomePage.css"
+import Pagination from './Pagination';
 
 const HomePage = () => {
 
     const [query, setQuery] = useState("");
     const [cities, setCities] = useState();
-    const [timeoutId, updateTimeOut] = useState()
+    const [limit, setLimit] = useState(5);
+    const [timeoutId, updateTimeOut] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [citiesPerPage] = useState(3);
 
     const debounce = (event) => {
         clearTimeout(timeoutId)
@@ -14,33 +18,45 @@ const HomePage = () => {
         updateTimeOut(timeout)
     }
 
-    const fetchUrl = async (query) => {
+    const fetchUrl = async (query,limit) => {
         const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?`;
 
         const res = await fetch(url + new URLSearchParams({
-            namePrefix: query
+            limit: limit,
+            namePrefix: query,
         }),{
             method: "GET",
             headers: {
-                "X-RapidAPI-Key": "d101490876msh46d4c72515dfee0p1e43fejsn5537c6de53c2",
+                "X-RapidAPI-Key": process.env.REACT_APP_GEODB_KEY,
                 "X-RapidAPI-Host" : 'wft-geo-db.p.rapidapi.com'
             },
         });
 
         const data = await res.json()
-        console.log(data)
+        //console.log(data)
         if(!data.errors){
             setCities(data.data);
         }
-    }  
+    }
 
+    console.log(cities)
+
+    const totalLimit = (e) => {
+        setLimit(e.target.value)
+    }
+
+    // const indexOfLastPage = currentPage * citiesPerPage;
+    // const indexOfFirstPage = indexOfLastPage - citiesPerPage;
+
+    // const currentCities = cities.slice(indexOfFirstPage, indexOfLastPage);
+
+    // const nPages = Math.ceil( (cities.length) / citiesPerPage );
 
     useEffect(()=>{
-        fetchUrl(query)
-        // eslint-disable-next-line
-    },[setQuery])
+        fetchUrl(query,limit)
+    },[query,limit])
 
-  return (
+    return (
     <>
         <div className='search-box'>
             <input type='text' placeholder='Search places ...' value={query} onChange={debounce} />
@@ -65,7 +81,9 @@ const HomePage = () => {
                                 <td>{id+1}</td>
                                 <td>{city.city}</td>
                                 <td>
-                                    <img src={`https://countryflagsapi.com/png/${city.country}`} alt='Country-Flag' className='flag'/>
+                                    <img src={`https://countryflagsapi.com/png/${city.country}`} alt='Country-Flag' className='flag'
+                                    loading='lazy'
+                                    />
                                 </td>
                             </tr>
                             )
@@ -76,6 +94,17 @@ const HomePage = () => {
             :
             <div className='no-data'>No data to display</div>
         }
+        <div className='page-limit'>
+            <div></div>
+            {/* <Pagination
+                nPages={nPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            /> */}
+            <div className='limit'>
+                <input type='number' min='1' max='10' value={limit} onChange={totalLimit} />
+            </div>
+        </div>
     </>
   )
 }
